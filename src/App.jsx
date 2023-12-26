ddc0402c93614ea57762d0dfd68788f86419dcac
MdNadiruzzamanNibras <nadiruzzamannibras@gmail.com>
2023-12-26 21:17:25 +0600

    font and color

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,44 @@ import { useState } from "react";
 
 
 const App = () => {
-  const [text, setText] = useState('10px')
+  const Permanent =['Permanent Marker', 'cursive']
+  const [text, setText] = useState('20px')
+  const [color, setColor] = useState('black')
+  const [font, setFont] = useState('')
   const handleChange = (event) => {
     setText(event.target.value);
+    
   };
-  console.log(text);
+  const colorChange = (event) => {
+    setColor(event.target.value);
+    
+  };
+  const fontChange = (event) => {
+    setFont(event.target.value);
+    
+  };
+  console.log(text, color);
   return (
     <div>
+      <div>
+         <select name="" id="" onChange={colorChange}>
+        <option value="black">black</option>
+        <option value="red">red</option>
+        <option value="blue">blue</option>
+      </select>
       <select onChange={handleChange}>
+        <option value="20px">20</option>
         <option value="50px">50</option>
         <option value="100px"> 100</option>
       </select>
-     <textarea style={{fontSize:`${text}`, width:"500px", height:"100px"}} name="" id="" ></textarea>
+      <select onChange={fontChange}>
+        <option value="">Regular</option>
+        <option value="Titillium Web">Titillium</option>
+        <option value='Roboto'> Roboto</option>
+        <option value={Permanent}> Permanent</option>
+      </select>
+     </div>
+     <textarea style={{fontSize:text, fontFamily:font, width:"1500px", height:"1000px", color:color}} name="" id="" ></textarea>
     </div>
   );
 };
