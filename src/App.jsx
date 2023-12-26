69fa0716bfb944a4dd66e0c0013dca451d8071df
MdNadiruzzamanNibras <nadiruzzamannibras@gmail.com>
2023-12-26 16:40:24 +0600

    text size

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
+import { useState } from "react";
 
 
 const App = () => {
+  const [text, setText] = useState('10px')
+  const handleChange = (event) => {
+    setText(event.target.value);
+  };
+  console.log(text);
   return (
     <div>
-      this app
+      <select onChange={handleChange}>
+        <option value="50px">50</option>
+        <option value="100px"> 100</option>
+      </select>
+     <textarea style={{fontSize:`${text}`, width:"500px", height:"100px"}} name="" id="" ></textarea>
     </div>
   );
 };
